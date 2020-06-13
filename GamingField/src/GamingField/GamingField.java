4a1dc5026ef566a35f82e57cb37f1527fbecb93a
stanislavchetinov7 <63706570+stanislavchetinov7@users.noreply.github.com>
2020-06-13 21:24:51 +0300

    Add files via upload

---
+++ GamingField/src/GamingField/GamingField.java
@@ -0,0 +1,154 @@
+package GamingField;
+import java.util.scanner;
+
+public class GamingField {
+
+public static void main(String[] args) {
+/* Задаваме String, защото искаме двата участника да бъдат постоянни в кода без да ги променяме
+*/
+String plr1 = new String("igrach1");
+String plr2 = new String("igrach2");
+
+Scanner scanner = new Scanner(System.in);
+
+
+}
+
+
+public class Invest {
+/* Задаваме integer за всяка една от командите, за да можем да ги вмъкнем в програмата	
+ */
+int cashInvested = 0;
+int minSum;
+int incomeMoney;
+int riskMoney;
+	
+
+}
+
+public class CalculateIncomeAndLost {
+	/* Задаваме integer за всяка една от командите, за да можем да ги вмъкнем в програмата	
+	 */
+int income;
+int lostMoney;
+int resultat;
+int investiciq;
+resultat = income + lostMoney;
+
+	
+}
+
+
+
+public class Steal {
+	
+boolean isTaken=false;	
+
+}
+public class Chance {
+	/* Задаваме String, защото искаме зарчето да бъде използвано с еднио от тези числа и for, за да вмъкнем него като команда.
+	*/	
+String[] dice = {"1", "2", "3", "4" , "5" , "6" , "7" ,  "8" , "9" , "10" };
+
+for (String i : dice) {
+
+System.out.println(i);
+}	
+	
+}
+
+public class PartyHard {
+	/* Задаваме integer , за да можем да ги вмъкнем в програмата	
+	 */	
+int kvadrat = 0;
+	/* Тук while функцията я използваме, за да покажем времето, докато комадата е активна.
+	 */
+while(kvadrat = 0) {
+	
+System.out.println("Gubite 25 shokoladovi parichki");
+}
+	
+}
+
+
+public class IgroviKvadratcheta {
+	
+int kapan = 0;
+while(kapan = 0) {
+	
+System.out.println("Kapanite deistvat");
+}
+
+
+public class EndGame {
+/* Тук boolean я използваме, за да покажем ако парите са вярни с определената стойност, те да се окажат правилни или неправилни ако е невярно в командата	
+ */
+boolean money = true;
+/* If я използваме за да допълним boolean функцията
+if ( money == true) {
+System.out.println("Igrata prodaljava");
+}
+else {
+System.out.println("Krai na igrata");
+} 
+
+
+
+
+	
+
+}
+
+}
+public class TheReshetka {
+	/* Задаваме String, защото искаме двата участника да бъдат постоянни в кода без да ги променяме
+	*/	
+int trap = 7;
+int invest = 3;
+int partyHard = 3;
+int chance = 3;
+int steal = 3;
+
+}
+
+public static void Zar (int zar, int movement) {
+	/* Тук while функцията я използваме, за да покажем времето, докато комадата е активна.
+	 */	
+while(zar >=10) {
+	
+System.out.println("Vzeto reshenie");	
+
+}
+
+
+
+for(int i = 1; i>= 10; i++) {
+	
+System.out.println("Vzeto reshenie ");
+	
+}
+
+
+
+	
+
+	
+
+
+
+	
+
+	
+}
+	
+
+
+	
+
+}
+
+	
+
+
+
+
